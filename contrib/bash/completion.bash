ae67a670f041608aedd246e95473d0ba95d296ea
Asbjørn Apeland <aude@users.noreply.github.com>
2020-10-15 00:20:41 +0200

    contrib: bash: completion.bash: complete option values

--- contrib/bash/completion.bash
+++ contrib/bash/completion.bash
@@ -3,6 +3,31 @@ _btrbk()
   local cur prev words cword split
   _init_completion -s || return
 
+  case "$prev" in
+    '-c' | '--config')
+      _filedir
+      return
+      ;;
+    '--exclude')
+      return
+      ;;
+    '-l' | '--loglevel')
+      COMPREPLY=($(compgen -W 'error warn info debug trace' -- "$cur"))
+      return
+      ;;
+    '--format')
+      COMPREPLY=($(compgen -W 'table long raw' -- "$cur"))
+      return
+      ;;
+    '--lockfile')
+      _filedir
+      return
+      ;;
+    '--override')
+      return
+      ;;
+  esac
+
   $split && return
 
   if [[ $cur == -* ]]; then
